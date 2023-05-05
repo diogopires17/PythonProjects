edaeb9614fd4406d5bad8534a5c7c548e187fe3a
diogopires17 <diogopires2001@gmail.com>
2023-05-05 14:34:09 +0100

    Done.

--- LoginSystem/main.py
+++ LoginSystem/main.py
@@ -2,6 +2,7 @@ import  os
 import hashlib
 clear = lambda: os.system('cls')
 
+
 def main():
     clear()
     print("Menu")
@@ -56,20 +57,60 @@ def Register():
                 elif error == 'eL' or error == 'E':
                     exit(1)
         addUserInfo([user, hash_password(password)]) 
+    print("\n")
+    print("Logged in!")
+
 def login():
-    pass
+    clear()
+    print("LOGIN")
+    print("---------------")
+    print("\n")
+    userinfo = {}
+    with open('userInfo.txt', 'r') as file:
+        for line in file:
+            line = line.split()
+            userinfo.update({line[0]:line[1]})
+    while True:    
+        username = input("Name")
+        username = sanitize(username)
+        if username not in userinfo:
+            print("Not registered")
+            print("\n")
+        else:
+            break
+    print("\n")
+    print("Logged in!")
+      
 
 def addUserInfo(userinfo: list):
-    pass
+    with open("userInfo.txt, 'a'") as file:
+        for info in userinfo:
+            file.write(info)
+            file.write(' ')
+        file.write('\n')
 
-def sanitize(user):
-    pass
+def sanitize(username):
+    username = username.split()
+    username = '-'.join(username)
+    return username
 
 def  userAlreadyExists(username, password):
-    pass
+    userinfo = {}
+    with open('userInfo.txt' , 'r') as file:
+        for line in file:
+            line = line.split()
+            if line[0] == username and line[1] == hash_password(password):
+                userinfo.update({line[0]:line[1]})
+    if userinfo == {}:
+        return False
+    return userinfo[username] == password
 
 def hash_password(password):
     return hashlib.sha256(str.encode)
 
 def check_pasword_hash(password, hash):
     return hash_password(password) == hash
+
+
+if __name__ == '__main__':
+    main()
